4fc342bc71c3a86071d5d0766e99555b514e748a
Pau Lòpez Pujals <iespaulopez@gmail.com>
2020-10-10 12:57:39 +0200

    Auth module request

--- modules/auth.js
+++ modules/auth.js
@@ -1,7 +1,11 @@
 const request = require('request');
 
 module.exports = {
-    login(){
-        
+    login(data){
+        request('ip:3000/login', function (error, response, body) {
+            console.error('error:', error);
+            console.log('statusCode:', response && response.statusCode);
+            console.log('body:', body);
+        });
     }
 }
